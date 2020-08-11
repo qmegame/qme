43ced937ef7b1e8c69d09729ad063db16f7a0b82
adamhutchings <adam.abahot@gmail.com>
2020-08-11 16:27:00 -0700

    Add framerate delays for refresh

--- src/org/qme/main/Main.java
+++ src/org/qme/main/Main.java
@@ -1,10 +1,23 @@
 package org.qme.main;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 	
-	public static void main(String[] args) {
+	public static final int FRAMERATE = 30;
+	
+	public static void main(String[] args) throws InterruptedException {
 		
-		System.out.println("QME!");
+		QApplication app = new QApplication();
+		
+		// Main loop time
+		while (true) {
+			
+			// Wait for some time
+			TimeUnit.MILLISECONDS.sleep(1000 / FRAMERATE);
+			
+			app.reload();
+		}
 		
 	}
 
