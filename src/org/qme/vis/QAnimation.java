67a774138d79cb8f57bb30f6b4bddf38600e0555
adamhutchings <adam.abahot@gmail.com>
2020-08-16 11:49:01 -0700

    Add QAnimation

---
+++ src/org/qme/vis/QAnimation.java
@@ -0,0 +1,74 @@
+package org.qme.vis;
+
+import org.qme.main.QApplication;
+import org.qme.main.QObject;
+
+/**
+ * This represents any sort of animation. How it
+ * works is it has a counter variable, and every update
+ * it calls a function that updates a certain object
+ * according to the counter variable.
+ * @author adamhutchings
+ * @since pre0
+ */
+public abstract class QAnimation extends QObject {
+	
+	/**
+	 * The object this animation acts on
+	 */
+	private QObject modified;
+	
+	/**
+	 * How long this object has been alive for
+	 */
+	private int count;
+	
+	/**
+	 * How long this object can be alive
+	 */
+	private int lifetime;
+	
+	/**
+	 * Calls the super constructor and initializes
+	 * lifetime and count
+	 * @param app the QApplication instance to bind to 
+	 * @param life the 
+	 */
+	public QAnimation(QApplication app, int life) {
+		
+		super(app);
+		
+		lifetime = life;
+		count = 0;
+		
+	}
+	
+	/**
+	 * This gets called to update the object.
+	 */
+	abstract protected void animate(int c, QObject q);
+
+	@Override
+	/**
+	 * Dispatches the appropriate animate call, and
+	 * also deletes the object (or readies it for GC)
+	 * if necessary.
+	 * @author adamhutchings
+	 * @since pre0
+	 */
+	public void update(QApplication app) {
+		
+		if (!(count < lifetime)) {
+			
+			// I hope this is the only reference necessary to remove.
+			app.objects.remove(this);
+			return;
+			
+		}
+		
+		animate(count, modified);
+		count++;
+		
+	}
+
+}
