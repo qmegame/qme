24612d0322ff49edf4ef28e7bf02032356910a6c
adamhutchings <adam.abahot@gmail.com>
2020-08-17 12:31:43 -0700

    Add world creation code

---
+++ src/org/qme/world/World.java
@@ -0,0 +1,56 @@
+package org.qme.world;
+
+import org.qme.main.QApplication;
+
+/**
+ * Represents a 2D array of tiles
+ * @author adamhutchings
+ * @since pre0
+ */
+public class World {
+	
+	/**
+	 * How long the x axis is
+	 */
+	public int xDimension;
+	
+	/**
+	 * How long the y axis is
+	 */
+	public int yDimension;
+	
+	/**
+	 * The actual tiles in the world
+	 */
+	public Tile[][] tiles;
+	
+	/**
+	 * Generates a world map
+	 * @author adamhutchings
+	 * @since pre0
+	 * @param x the x size of the world
+	 * @param y the y size of the world
+	 */
+	public World(QApplication a, int x, int y) {
+		
+		xDimension = x;
+		yDimension = y;
+		
+		TileType[][] map = WorldGen.newWorldMap(x, y);
+		
+		tiles = new Tile[x][y];
+		
+		// Initialize all of the tiles
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				
+				tiles[i][j] = new Tile(a, i, j);
+				
+				tiles[i][j].setType(map[i][j]);
+				
+			}
+		}
+		
+	}
+
+}
