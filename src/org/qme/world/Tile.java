207d2f3517124dc43b27be53d3aa05a7ffc46196
adamhutchings <adam.abahot@gmail.com>
2020-08-17 12:22:09 -0700

    Finish rendering code

--- src/org/qme/world/Tile.java
+++ src/org/qme/world/Tile.java
@@ -1,5 +1,12 @@
 package org.qme.world;
 
+import java.awt.Graphics;
+
+import org.qme.main.QApplication;
+import org.qme.main.QObject;
+import org.qme.vis.Perspective;
+import org.qme.vis.QRenderable;
+
 /**
  * A tile in the world. For now, just has a type
  * and a location. Nothing big.
@@ -7,7 +14,7 @@ package org.qme.world;
  * @since pre0
  * @see org.qme.world.TileType
  */
-public class Tile {
+public class Tile extends QObject implements QRenderable {
 	
 	// Location
 	int x, y;
@@ -20,7 +27,8 @@ public class Tile {
 	 * @param x x-coordinate of tile.
 	 * @param y y-coordinate of tile.
 	 */
-	public Tile(int x, int y) {
+	public Tile(QApplication app, int x, int y) {
+		super(app);
 		this.x = x;
 		this.y = y;
 	}
@@ -39,6 +47,67 @@ public class Tile {
 	 */
 	public void setType(TileType type) {
 		this.type = type;
+	}
+	
+	/**
+	 * Nothing for now
+	 */
+	public void update(QApplication app) {}
+	
+	@Override
+	/**
+	 * Draws a square representing the tile.
+	 */
+	public void render(Graphics g) {
+		
+		// Getting the right color (this is just a placeholder, we'll have real textures later)
+		switch (type) {
+		
+		case UNGENERATED:
+			g.setColor(TileColors.UNGENERATED_COLOR);
+			break;
+			
+		case OCEAN:
+			g.setColor(TileColors.OCEAN_COLOR);
+			break;
+			
+		case SEA:
+			g.setColor(TileColors.SEA_COLOR);
+			break;
+			
+		case PLAINS:
+			g.setColor(TileColors.PLAINS_COLOR);
+			break;
+			
+		case DESERT:
+			g.setColor(TileColors.DESERT_COLOR);
+			break;
+			
+		case FOREST:
+			g.setColor(TileColors.FOREST_COLOR);
+			break;
+			
+		case MOUNTAIN:
+			g.setColor(TileColors.MOUNTAIN_COLOR);
+			break;
+			
+		case HIGH_MOUNTAIN:
+			g.setColor(TileColors.HIGH_MOUNTAIN_COLOR);
+			break;
+			
+		case FERTILE_PLAINS:
+			g.setColor(TileColors.FERTILE_PLAINS_COLOR);
+			break;
+		
+		}
+		
+		// Now, let's draw.
+		g.drawRect(
+			(x * Perspective.TILE_SIZE) - application.qiscreen.xOffset,
+			(y * Perspective.TILE_SIZE) - application.qiscreen.yOffset,
+			Perspective.TILE_SIZE, Perspective.TILE_SIZE
+		);
+		
 	}
 
 }
