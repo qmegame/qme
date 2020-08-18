df9a864346143f75ae0c288b5506696c27f93890
adamhutchings <adam.abahot@gmail.com>
2020-08-18 16:26:10 -0700

    Add UIComponent inheritance

--- src/org/qme/world/Tile.java
+++ src/org/qme/world/Tile.java
@@ -1,11 +1,13 @@
 package org.qme.world;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import org.qme.main.QApplication;
 import org.qme.main.QObject;
 import org.qme.vis.Perspective;
 import org.qme.vis.QRenderable;
+import org.qme.vis.ui.UIComponent;
 
 /**
  * A tile in the world. For now, just has a type
@@ -14,7 +16,7 @@ import org.qme.vis.QRenderable;
  * @since pre0
  * @see org.qme.world.TileType
  */
-public class Tile extends QObject implements QRenderable {
+public class Tile extends QObject implements QRenderable, UIComponent {
 	
 	// Location
 	int x, y;
@@ -52,7 +54,18 @@ public class Tile extends QObject implements QRenderable {
 	/**
 	 * Nothing for now
 	 */
-	public void update(QApplication app) {}
+	public void update(QApplication app) {
+		
+	}
+	
+	/**
+	 * Get the bounding rectangle
+	 */
+	public Rectangle getRect() {
+		return new Rectangle((x * Perspective.TILE_SIZE) + 10 - application.qiscreen.xOffset,
+				(y * Perspective.TILE_SIZE) + 10 - application.qiscreen.yOffset,
+				Perspective.TILE_SIZE - 10, Perspective.TILE_SIZE - 10);
+	}
 	
 	@Override
 	/**
@@ -101,13 +114,37 @@ public class Tile extends QObject implements QRenderable {
 		
 		}
 		
+		Rectangle rect = getRect();
+		
 		// Now, let's draw.
 		g.fillRect(
-			(x * Perspective.TILE_SIZE) + 10 - application.qiscreen.xOffset,
-			(y * Perspective.TILE_SIZE) + 10 - application.qiscreen.yOffset,
-			Perspective.TILE_SIZE - 10, Perspective.TILE_SIZE - 10
+			rect.x, rect.y, rect.width, rect.height
 		);
 		
+	}
+	
+	@Override
+	/**
+	 * Does nothing
+	 */
+	public void mouseClickOff() {
+		
+	}
+	
+	@Override
+	/**
+	 * Does nothing
+	 */
+	public void mouseClickOn() {
+		
+	}
+	
+	@Override
+	/**
+	 * Returns whether said click is inside the tile.
+	 */
+	public boolean clickIsIn(int x, int y) {
+		return getRect().contains(x, y);
 	}
 
 }
