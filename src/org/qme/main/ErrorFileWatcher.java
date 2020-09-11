64919812700dafcc8bb2c10f6d9ba41dc761a9f7
adamhutchings <adam.abahot@gmail.com>
2020-09-11 12:39:57 -0700

    Add complete file paths and spaces

--- src/org/qme/main/ErrorFileWatcher.java
+++ src/org/qme/main/ErrorFileWatcher.java
@@ -21,7 +21,7 @@ public class ErrorFileWatcher {
 	public void check() {
 		long actualLastTime = this.watched.lastModified();
 		if(actualLastTime != this.lastTime) {
-			JOptionPane.showMessageDialog(null, "An error has occurred. Please find your " + Main.ERROR_LOG + "file and send the error message to a dev.");
+			JOptionPane.showMessageDialog(null, "An error has occurred. Please find your " + System.getProperty("user.dir") + "/" + Main.ERROR_LOG + " file and send the error message to a dev.");
 			System.exit(0);
 		}
 	}
