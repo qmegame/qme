90e6e68f6b3393b58f5fa1866f5f4c9ce9c92469
adamhutchings <adam.abahot@gmail.com>
2020-09-15 16:45:46 -0700

    Rename file

---
+++ src/org/qme/logging/FileWatcher.java
@@ -0,0 +1,32 @@
+package org.qme.logging;
+
+import java.io.File;
+
+import static org.qme.logging.QLoggingUtils.FS;
+import static org.qme.logging.QLoggingUtils.QDATA;
+import static org.qme.main.Main.displayError;
+
+/**
+ * @author S-Mackenzie1678
+ * @since pre2
+ */
+public class FileWatcher {
+	
+	public static final String ERROR_LOG = QDATA + FS + "error_logs.txt";
+	
+	private long lastTime;
+	private File watched;
+	
+	public FileWatcher(File file) {
+		this.watched = file;
+		this.lastTime = file.lastModified();
+	}
+	
+	public void check() {
+		long actualLastTime = this.watched.lastModified();
+		if(actualLastTime != this.lastTime) {
+			displayError("An error has occurred. Please find your " + System.getProperty("user.dir") + FS + ERROR_LOG + " file and send the error message to a dev.");
+			System.exit(0);
+		}
+	}
+}
