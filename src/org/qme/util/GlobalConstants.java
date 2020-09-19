360269b802ad5b30beedcf2c4faeb111e342f094
adamhutchings <adam.abahot@gmail.com>
2020-09-18 23:41:53 -0700

    Create some global constants

---
+++ src/org/qme/util/GlobalConstants.java
@@ -0,0 +1,25 @@
+package org.qme.util;
+
+/**
+ * This exists for the purpose of keeping all
+ * global variables in one place.
+ * @author adamhutchings
+ * @since pre3
+ */
+public class GlobalConstants {
+	
+	/**
+	 * This represents how often the screen is reloaded.
+	 * Moved from org.qme.main in development for pre3.
+	 * @author adamhutchings
+	 * @since pre0
+	 */
+	public static final int FRAMERATE = 30;
+	
+	/**
+	 * Whether tooltips are on.
+	 * @since pre3
+	 */
+	public static boolean TOOLTIPS = true;
+
+}
