d4bc631a252ff11eb354195f9aae4967c1479e16
adamhutchings <adam.abahot@gmail.com>
2020-09-23 15:53:01 -0700

    Finish code

--- src/org/qme/vis/ui/TechViewer.java
+++ src/org/qme/vis/ui/TechViewer.java
@@ -1,10 +1,15 @@
 package org.qme.vis.ui;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import org.qme.main.GlobalState;
 import org.qme.main.QApplication;
 import org.qme.main.QObject;
+import org.qme.tech.Tech;
 import org.qme.vis.QLayer;
 import org.qme.vis.QRenderable;
 
@@ -14,9 +19,25 @@ import org.qme.vis.QRenderable;
  * @since pre3
  */
 public class TechViewer extends QObject implements QRenderable, UIComponent {
+	
+	private static int TECH_VIEW_WIDTH = 200;
+	private static int TECH_VIEW_HEIGHT = 50;
+	
+	/**
+	 * A rectangular background for the tech
+	 */
+	private Rectangle backgroundRect;
+	
+	// Which tech it is
+	public Tech tech;
 
-	public TechViewer(QApplication app) {
+	public TechViewer(QApplication app, Tech t, int xPos, int yPos) {
 		super(app);
+		tech = t;
+		backgroundRect = new Rectangle(
+			xPos - TECH_VIEW_WIDTH / 2, yPos = TECH_VIEW_HEIGHT / 2,
+			TECH_VIEW_WIDTH, TECH_VIEW_HEIGHT
+		);
 	}
 
 	@Override
@@ -46,7 +67,7 @@ public class TechViewer extends QObject implements QRenderable, UIComponent {
 
 	@Override
 	public boolean clickIsIn(int x, int y) {
-		return false;
+		return backgroundRect.contains(x, y);
 	}
 
 	@Override
@@ -56,7 +77,20 @@ public class TechViewer extends QObject implements QRenderable, UIComponent {
 
 	@Override
 	public void render(Graphics g) {
-		// Nothing
+		
+		// Render the rectangle base (will be different colors later)
+		g.setColor(Color.GRAY);
+		g.drawRect(backgroundRect.x, backgroundRect.y, backgroundRect.width, backgroundRect.height);
+		
+		// Draw the outline (THICC)
+		if (this.hoveredOver) {
+			g.setColor(Color.BLACK);
+			((Graphics2D) g).setStroke(new BasicStroke(10));
+		}
+		
+		// Write the name of the tech
+		UIUtils.drawCenteredString(g, tech.getName(), backgroundRect, UIUtils.QME_FONT);
+		
 	}
 
 	@Override
