630d72ed6f76f4afa82fb1fe6dce413b0e2d508e
adamhutchings <adam.abahot@gmail.com>
2020-09-23 16:00:09 -0700

    Add (small) tech menu

---
+++ src/org/qme/menus/TechViewMenu.java
@@ -0,0 +1,36 @@
+package org.qme.menus;
+
+import org.qme.tech.Tech;
+import org.qme.vis.ui.QButton;
+import org.qme.vis.ui.TechViewer;
+
+import static org.qme.util.GlobalConstants.SCREEN_HEIGHT;
+import static org.qme.util.GlobalConstants.SCREEN_WIDTH;
+
+import org.qme.main.GlobalState;
+import org.qme.main.QApplication;
+
+public class TechViewMenu {
+	
+	public static void makeMenu(QApplication app) {
+		
+		new TechViewer(app, Tech.AGRICULTURE_TECH, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+		
+		// Quit time
+		new QButton(app, SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 100, "Return to game") {
+
+			@Override
+			public void mouseClickOff() {
+				app.setState(GlobalState.MAIN_GAME);
+			}
+			
+			@Override
+			public GlobalState getActiveState() {
+				return GlobalState.TECH_TREE_VIEW;				
+			}
+			
+		};
+		
+	}
+
+}
