a5c7f45f3d4033608d0d5325e54691e545796695
adamhutchings <adam.abahot@gmail.com>
2020-09-26 11:13:11 -0700

    Make Settlement basics

---
+++ src/org/qme/structure/Settlement.java
@@ -0,0 +1,16 @@
+package org.qme.structure;
+
+import org.qme.player.PoliticalEntity;
+
+/**
+ * Just the basics of what I assume a settlement might need.
+ * @author adamhutchings
+ * @since pre3
+ */
+public class Settlement {
+
+	public int x, y;
+	
+	public PoliticalEntity directOwner;
+	
+}
