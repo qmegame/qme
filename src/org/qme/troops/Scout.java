01c4ba643b37d3e91a4feed1292e0c095fce678f
adamhutchings <adam.abahot@gmail.com>
2020-10-23 10:20:19 -0700

    Remove unneeded overrides

--- src/org/qme/troops/Scout.java
+++ src/org/qme/troops/Scout.java
@@ -1,8 +1,5 @@
 package org.qme.troops;
 
-import java.awt.Graphics;
-
-import org.qme.main.GlobalState;
 import org.qme.main.QApplication;
 import org.qme.vis.QLayer;
 import org.qme.world.Tile;
@@ -27,36 +24,6 @@ public class Scout extends Unit {
         } else {
             return yDistance;
         }
-    }
-
-    @Override
-    public QLayer getLayer() {
-        // TODO Auto-generated method stub
-        return QLayer.TROOP_LAYER;
-    }
-
-    @Override
-    public void render(Graphics g) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public GlobalState getActiveState() {
-        // TODO Auto-generated method stub
-    	return GlobalState.MAIN_GAME;
-    }
-
-    @Override
-    public boolean clickIsIn(int x, int y) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void update(QApplication app) {
-        // TODO Auto-generated method stub
-
     }
     
 }
