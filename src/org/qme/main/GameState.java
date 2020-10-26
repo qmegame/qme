846415e267ed6144e3d04e3d8c927f5b1f33a3f3
adamhutchings <adam.abahot@gmail.com>
2020-10-25 20:57:01 -0700

    Add calls to action() from Structure

--- src/org/qme/main/GameState.java
+++ src/org/qme/main/GameState.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.qme.ai.AIMain;
 import org.qme.player.PoliticalEntity;
+import org.qme.structure.Structure;
 
 /**
  * This is the class that will be written to saves eventually
@@ -25,16 +26,29 @@ public final class GameState {
 	}
 	
 	public void turnEnded() {
+		
 		this.playerTurn++;
 		this.playerTurn %= this.civilizations.size();
 		if (this.playerTurn == 0) {
 			this.turn++;
 		}
+		
+		// Update the structures.
+		for (QObject qo : owner.objects) {
+			if (qo instanceof Structure) {
+				// Move if the turn is boutta happen
+				if ( ((Structure) qo).owner.directOwner == civilizations.get(playerTurn)) {
+					((Structure) qo).action();
+				}
+			}
+		}
+		
 		// If it's an AI move, use the ai move thingy
 		if (civilizations.get(playerTurn).ai) {
 			AIMain.makeMove(civilizations.get(playerTurn), this);
 			// Risk of recursion here, so at least one human player must exist.
 			turnEnded();
 		}
+		
 	}
 }
