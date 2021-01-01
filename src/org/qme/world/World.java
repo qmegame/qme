deaab7e41c2a95249d5181ae94f94280ec804c02
adamhutchings <adam.abahot@gmail.com>
2021-01-01 00:52:30 -0800

    Comment out unused number

--- src/org/qme/world/World.java
+++ src/org/qme/world/World.java
@@ -10,7 +10,7 @@ public class World {
 	/**
 	 * The size of the world
 	 */
-	private static final int WORLD_SIZE = 25;
+	// private static final int WORLD_SIZE = 25;
 	
 	/**
 	 * All tiles in the world.
@@ -22,14 +22,27 @@ public class World {
 	 */
 	public World() {
 		
+		// BROKEN CODE - WORLD GENERATION FAULTY
+		/*
 		tiles = new Tile[WORLD_SIZE][WORLD_SIZE];
-		
 		TileType[][] typelist = WorldGenerator.generateWorldMap(WORLD_SIZE);
 		for (int i = 0; i < WORLD_SIZE; i++) {
 			for (int j = 0; j < WORLD_SIZE; j++) {
 				tiles[i][j] = new Tile(i, j, typelist[i][j]);
 			}
 		}
+		*/
+		tiles = new Tile[1][1];
+		tiles[0][0] = new Tile(0, 0, TileType.DESERT);
+		
+	}
+	
+	/**
+	 * Get all the tiles.
+	 * @return the tile map for rendering
+	 */
+	public Tile[][] getTiles() {
+		return tiles;
 	}
 
 }
