c8e3eb82b3b2b0c488808dfeac5573d7ea50e6ea
adamhutchings <adam.abahot@gmail.com>
2020-12-31 22:26:41 -0800

    Add world type

---
+++ src/org/qme/world/World.java
@@ -0,0 +1,32 @@
+package org.qme.world;
+
+/**
+ * Represents a world in the game, with tiles.
+ * @author adamhutchings
+ * @since preA
+ */
+public class World {
+	
+	/**
+	 * The size of the world
+	 */
+	private static final int WORLD_SIZE = 25;
+	
+	/**
+	 * All tiles in the world.
+	 */
+	private final Tile[][] tiles;
+	
+	public World() {
+		
+		tiles = new Tile[WORLD_SIZE][WORLD_SIZE];
+		
+		TileType[][] typelist = WorldGenerator.generateWorldMap(WORLD_SIZE);
+		for (int i = 0; i < WORLD_SIZE; i++) {
+			for (int j = 0; j < WORLD_SIZE; j++) {
+				tiles[i][j] = new Tile(i, j, typelist[i][j]);
+			}
+		}
+	}
+
+}
