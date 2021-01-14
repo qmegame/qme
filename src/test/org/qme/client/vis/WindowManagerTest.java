a7d5bae24e8d731cf65b3a4fc3758160603f0232
Tom <treez.zhang@gmail.com>
2021-01-14 13:11:12 -0800

    debug and JUnit

---
+++ src/test/org/qme/client/vis/WindowManagerTest.java
@@ -0,0 +1,50 @@
+package org.qme.client.vis;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindowManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shouldBeOpen() {
+    }
+
+    @Test
+    void repaint() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void getOffsets() {
+    }
+
+    @Test
+    void getMouseLocation() {
+    }
+
+    @Test
+    void getWindowX() {
+    }
+
+    @Test
+    void getWindowY() {
+    }
+
+    @Test
+    void addObject() {
+    }
+}
