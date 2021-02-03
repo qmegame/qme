bacf418fd2ffc5002b422136b33780c8561bb0ac
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-02-03 13:19:03 -0800

    Add javadoc

--- src/main/java/org/qme/io/AudioFiles.java
+++ src/main/java/org/qme/io/AudioFiles.java
@@ -1,5 +1,10 @@
 package org.qme.io;
 
+/**
+ * A place to store the filepaths of audio files
+ * @author jakeroggenbuck
+ * @since 0.3.0
+ */
 public class AudioFiles {
     public static String menu = "src/main/resources/sounds/music/QME5_Menu.wav";
     public static String evident = "src/main/resources/sounds/music/Evident_10520.wav";
--- src/main/java/org/qme/io/AudioPlayerState.java
+++ src/main/java/org/qme/io/AudioPlayerState.java
@@ -1,5 +1,10 @@
 package org.qme.io;
 
+/**
+ * The state of the audio player
+ * @author jakeroggenbuck
+ * @since 0.3.0
+ */
 public enum AudioPlayerState {
     PLAY,
     PAUSED,
