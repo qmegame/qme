d5767e3c4d5237f8d8eef6ed0f7f6ab898f25143
Cameron <cjpuffnstuff@gmail.com>
2021-02-02 17:08:48 -0800

    Removed get height method to fix potential merge conflics

--- src/main/java/org/qme/client/vis/gui/QFont.java
+++ src/main/java/org/qme/client/vis/gui/QFont.java
@@ -43,10 +43,6 @@ public class QFont {
         loadFont();
     }
 
-    public int getHeight() {
-        return fontHeight;
-    }
-
     /**
      * Generates a texture for the font and saves it as an ID
      * If multiple fonts are using the same name a warning will be printed and only 1 will be loaded
