3c91119689af2ca7f3cb29806099fa0f107a4550
adamhutchings <adam.abahot@gmail.com>
2021-02-05 00:37:37 -0800

    Add probabilities

--- src/main/java/org/qme/world/res/AbstractResource.java
+++ src/main/java/org/qme/world/res/AbstractResource.java
@@ -20,11 +20,60 @@ public class AbstractResource {
 
     /**
      * Spawn chance is a whole number, 0-100
-     * @param type which tile it spawns on
+     * @param tileType which tile it spawns on
      * @return
      */
-    public int getSpawnChance(TileType type) {
-        return 0;
+    public int getSpawnChance(TileType tileType) {
+        return switch (this.type) {
+            case COAL ->
+                    switch (tileType) {
+                        case MOUNTAIN -> 50;
+                        case HIGH_MOUNTAIN -> 20;
+                        default -> 0;
+                    };
+            case FRUIT ->
+                    switch (tileType) {
+                        case FOREST -> 20;
+                        default -> 0;
+                    };
+            case GOLD_ORE ->
+                    switch (tileType) {
+                        case MOUNTAIN -> 10;
+                        case HIGH_MOUNTAIN -> 20;
+                        default -> 0;
+                    };
+            case GRAPE ->
+                    switch (tileType) {
+                        case FERTILE_PLAINS -> 10;
+                        default -> 0;
+                    };
+            case GRASS ->
+                    switch (tileType) {
+                        case PLAINS -> 70;
+                        default -> 0;
+                    };
+            case LEAF ->
+                    switch (tileType) {
+                        case FOREST -> 80;
+                        default -> 0;
+                    };
+            case SALT ->
+                    switch (tileType) {
+                        case OCEAN -> 200;
+                        default -> 0;
+                    };
+            case SEA_WEED ->
+                    switch (tileType) {
+                        case OCEAN -> 30;
+                        case SEA -> 25;
+                        default -> 0;
+                    };
+            case TALL_GRASS ->
+                    switch (tileType) {
+                        case FERTILE_PLAINS -> 50;
+                        default -> 0;
+                    };
+        };
     }
 
     public ResourceType getType() {
