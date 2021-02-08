85dad14ad6aa27f33d391e0df2cbf2c1591711c6
adamhutchings <adam.abahot@gmail.com>
2021-02-07 16:30:50 -0800

    Add some javadoc

---
+++ src/main/java/org/qme/init/PreInit.java
@@ -0,0 +1,11 @@
+package org.qme.init;
+
+/**
+ * This class contains the utilities for making sure the environment is set up
+ * correctly, and initializes logging and other necessary I/O. Before
+ * PreInit.init is called, logging capabilities do not exist.
+ * @author adamhutchings
+ * @since 0.4
+ */
+public class PreInit {
+}
