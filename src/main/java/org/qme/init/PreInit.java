36e96991772757f2038378f4affb2290aef1aa49
adamhutchings <adam.abahot@gmail.com>
2021-02-07 20:02:55 -0800

    Add pre-init initialization

--- src/main/java/org/qme/init/PreInit.java
+++ src/main/java/org/qme/init/PreInit.java
@@ -1,5 +1,10 @@
 package org.qme.init;
 
+import org.qme.io.Logger;
+import org.qme.io.Severity;
+
+import java.io.File;
+
 /**
  * This class contains the utilities for making sure the environment is set up
  * correctly, and initializes logging and other necessary I/O. Before
@@ -8,4 +13,29 @@ package org.qme.init;
  * @since 0.4
  */
 public class PreInit {
+
+    private static String QDATA = "qdata/";
+
+    /**
+     * No initialization!
+     */
+    private PreInit() { throw new IllegalStateException("PreInit"); }
+
+    /**
+     * Initialize log files.
+     */
+    public void init() {
+
+        try {
+            new File("qdata/").mkdir();
+        } catch(SecurityException e) {
+            System.exit(-1); // TODO
+        }
+
+        Logger.logsActivated = true;
+
+        Logger.log("Successfully initialized logger, pre-init done.", Severity.NORMAL);
+
+    }
+
 }
