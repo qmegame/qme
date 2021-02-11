ab5b25008780ba0b9cc13541b3343935ce337c5d
Cameron <cjpuffnstuff@gmail.com>
2021-02-11 12:37:42 -0800

    Add spawn tables for resources

--- src/main/java/org/qme/world/res/Resource.java
+++ src/main/java/org/qme/world/res/Resource.java
@@ -1,8 +1,12 @@
 package org.qme.world.res;
 
-import org.qme.io.Logger;
-import org.qme.io.Severity;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.qme.client.vis.tex.TextureManager;
 import org.qme.world.TileType;
+
+import java.io.InputStream;
+import java.util.HashMap;
 
 /**
  * An abstract class for resources
@@ -16,115 +20,59 @@ public class Resource {
      */
     public ResourceType type;
 
+    private static final HashMap<ResourceType, HashMap<TileType, Integer>> spawnChances = loadLootTable();
+
     public Resource(ResourceType type) {
         this.type = type;
     }
 
     /**
      * Spawn chance is a whole number, 0-100
-     * @return
+     * @param type the resource to check
+     * @param tileType the tile to check
+     * @return the chance of spawning
      */
     public static int getSpawnChance(ResourceType type, TileType tileType) {
-        return switch (type) {
-            case COAL ->
-                    switch (tileType) {
-                        case MOUNTAIN -> 50;
-                        case HIGH_MOUNTAIN -> 20;
-                        default -> 0;
-                    };
-            case FRUIT ->
-                    switch (tileType) {
-                        case FOREST -> 20;
-                        default -> 0;
-                    };
-            case GOLD_ORE ->
-                    switch (tileType) {
-                        case MOUNTAIN -> 10;
-                        case HIGH_MOUNTAIN -> 20;
-                        default -> 0;
-                    };
-            case GRAPE_VINE ->
-                    switch (tileType) {
-                        case FERTILE_PLAINS -> 10;
-                        default -> 0;
-                    };
-            case GRAPES ->
-                    switch (tileType) {
-                        case FERTILE_PLAINS -> 5;
-                        default -> 0;
-                    };
-            case WHEAT ->
-                    switch (tileType) {
-                        case FERTILE_PLAINS -> 20;
-                        default -> 0;
-                    };
-            case GRASS ->
-                    switch (tileType) {
-                        case PLAINS -> 70;
-                        default -> 0;
-                    };
-            case LEAF ->
-                    switch (tileType) {
-                        case FOREST -> 80;
-                        default -> 0;
-                    };
-            case LUMBER ->
-                    switch (tileType) {
-                        case FOREST -> 65;
-                        default -> 0;
-                    };
-            case SEAGULL ->
-                    switch (tileType) {
-                        case OCEAN -> 2;
-                        case SEA -> 6;
-                        default -> 0;
-                    };
-            case SALT ->
-                    switch (tileType) {
-                        case OCEAN -> 20; // Settings this to 20 percent because it looks weird otherwise. This should be set back to 200% once seaweed and fish area added
-                        default -> 0;
-                    };
-            case SEA_WEED ->
-                    switch (tileType) {
-                        case OCEAN -> 30;
-                        case SEA -> 25;
-                        default -> 0;
-                    };
-           case TALL_GRASS ->
-                    switch (tileType) {
-                        case FERTILE_PLAINS -> 50;
-                        default -> 0;
-                    };
-            case ROCK ->
-                    switch (tileType) {
-                        case HIGH_MOUNTAIN -> 35;
-                        case MOUNTAIN -> 80;
-                        default -> 0;
-                    };
-            case SNOW ->
-                    switch (tileType) {
-                        case HIGH_MOUNTAIN -> 100;
-                        case MOUNTAIN -> 20;
-                        default -> 0;
-                    };
-            case SAND ->
-                    switch (tileType) {
-                        case DESERT -> 60;
-                        default -> 0;
-                    };
-            case SAND_STONE ->
-                    switch (tileType) {
-                        case DESERT -> 5;
 
-                        default -> 0;
-                    };
-            case CACTUS ->
-                    switch (tileType) {
-                        case DESERT -> 20;
-                        default -> 0;
-                    };
+        if (!spawnChances.containsKey(type)) {
+            return 0;
+        }
 
-        };
+        if (!spawnChances.get(type).containsKey(tileType)) {
+            return 0;
+        }
+
+        return spawnChances.get(type).get(tileType);
+    }
+
+    /**
+     * Loads the loot tables from spawn_tables.json
+     * @return loaded loot tables
+     */
+    private static HashMap<ResourceType, HashMap<TileType, Integer>> loadLootTable() {
+
+        HashMap<ResourceType, HashMap<TileType, Integer>> toReturn = new HashMap<>();
+
+        InputStream stream = TextureManager.class.getResourceAsStream("/misc/spawn_tables.json");
+        JSONObject types = new JSONObject(new JSONTokener(stream));
+
+        if (types == null) {
+            return toReturn;
+        }
+
+        for (String resourceType : types.keySet()) {
+            JSONObject chances = types.getJSONObject(resourceType);
+
+            HashMap<TileType, Integer> chancesMap = new HashMap<>();
+
+            for (String tileType : chances.keySet()) {
+                chancesMap.put(TileType.valueOf(tileType.toUpperCase()), chances.getInt(tileType));
+            }
+
+            toReturn.put(ResourceType.valueOf(resourceType.toUpperCase()), chancesMap);
+        }
+
+        return toReturn;
     }
 
     public ResourceType getType() {
