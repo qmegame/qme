3a5882235eb882d6fdb29569ab961138fffac9b2
adamhutchings <adam.abahot@gmail.com>
2021-03-14 17:05:28 -0700

    Add main menu

--- src/main/java/org/qme/client/vis/gui/GUIManager.java
+++ src/main/java/org/qme/client/vis/gui/GUIManager.java
@@ -1,6 +1,7 @@
 package org.qme.client.vis.gui;
 
 import org.qme.client.vis.gui.ui.DebugUI;
+import org.qme.client.vis.gui.ui.MenuUI;
 import org.qme.client.vis.gui.ui.ProfilerUI;
 import org.qme.client.vis.gui.ui.ResourcesUI;
 
@@ -12,14 +13,21 @@ public class GUIManager {
     public static ResourcesUI resourcesUI;
     public static DebugUI debugUI;
     public static ProfilerUI profilerUI;
+    public static MenuUI menuUI;
 
     /**
      * Load everything.
      */
     public static void loadGUIs() {
+
         resourcesUI = new ResourcesUI();
         debugUI = new DebugUI();
         profilerUI = new ProfilerUI();
+        menuUI = new MenuUI();
+
+        // make main menu visible
+        menuUI.show();
+
     }
 
 }
