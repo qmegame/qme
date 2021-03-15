e3b80024d07b1d5923830e9988b2b40bb06c0845
adamhutchings <adam.abahot@gmail.com>
2021-03-14 17:19:28 -0700

    Add Game class

---
+++ src/main/java/org/qme/game/Game.java
@@ -0,0 +1,32 @@
+package org.qme.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    List<Player> playerList = new ArrayList<>();
+
+    private int currentPlayerIndex = 0, turnCount = 0;
+
+    public static Game game = new Game();
+
+    public Game() {
+
+        // For now, one player.
+        playerList.add(new Player());
+
+    }
+
+    public Player currentPlayer() {
+        return playerList.get(currentPlayerIndex);
+    }
+
+    public void nextPlayer() {
+        if (++currentPlayerIndex == playerList.size()) {
+            currentPlayerIndex = 0;
+            ++turnCount;
+        }
+    }
+
+}
