3652c311d342f906e122638b5d41a7a0a9be369d
Cameron <cjpuffnstuff@gmail.com>
2021-03-14 23:50:41 -0700

    Crash reports yeeeeey

---
+++ src/main/java/org/qme/io/Crash.java
@@ -0,0 +1,21 @@
+package org.qme.io;
+
+public class Crash {
+
+    public Exception exception;
+    public String description;
+    public boolean isFatal;
+
+    /**
+     * Generates a crash report given an exception
+     * @param exception The exception that caused this crash report
+     * @param description A description of the cause of the grash
+     * @param isFatal should the logging of this crash report terminate the program
+     */
+    public Crash(Exception exception, String description, boolean isFatal) {
+        this.exception = exception;
+        this.description = description;
+        this.isFatal = isFatal;
+    }
+
+}
