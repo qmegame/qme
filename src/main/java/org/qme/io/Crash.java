1a60dd5b18e41a4a9bd4347c9a5583651c8bd6ff
Cameron <cjpuffnstuff@gmail.com>
2021-03-15 22:29:08 -0700

    Fix type

--- src/main/java/org/qme/io/Crash.java
+++ src/main/java/org/qme/io/Crash.java
@@ -9,7 +9,7 @@ public class Crash {
     /**
      * Generates a crash report given an exception
      * @param exception The exception that caused this crash report
-     * @param description A description of the cause of the grash
+     * @param description A description of the cause of the crash
      * @param isFatal should the logging of this crash report terminate the program
      */
     public Crash(Exception exception, String description, boolean isFatal) {
