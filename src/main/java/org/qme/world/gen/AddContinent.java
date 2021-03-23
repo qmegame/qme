4d305da636493100fe715628cc144a46d3b9f0a9
Tom <treez.zhang@gmail.com>
2021-03-23 11:42:24 -0700

    addContinent restructure

--- src/main/java/org/qme/world/gen/AddContinent.java
+++ src/main/java/org/qme/world/gen/AddContinent.java
@@ -1,4 +1,187 @@
 package org.qme.world.gen;
 
-public class AddContinent {
+import org.qme.io.Logger;
+import org.qme.io.Severity;
+import org.qme.world.TileType;
+
+public class AddContinent extends WorldGenerator{
+    /**
+     * A utility function to generate and add a continent
+     * @author santiago, tom
+     * @since 0.1.0
+     * @param world The array to be modified
+     * @param side The size of the world
+     * @param centerX The center of the continent
+     * @param centerY The center of the continent
+     * @return The inputted array with a continent added
+     */
+    static TileType[][] addContinent(TileType[][] world,
+                                     double side, int centerX, int centerY) {
+
+        // Set up return world
+        TileType[][] newWorld;
+        newWorld = world;
+
+        Logger.log("assign center tile to random", Severity.DEBUG);
+        // Assign center tile to a random non-mountain land type
+        newWorld[centerX][centerY] = WorldGenerator.assignRandomFlatLand();
+        Logger.log("centerX is: " + Integer.toString(centerX), Severity.DEBUG);
+        Logger.log("centerY is: " + Integer.toString(centerY), Severity.DEBUG);
+
+        // Set up extreme points with most extreme values
+        int leftmost = 0;
+        int upmost = 0;
+        int rightmost = (int) (side - 1);
+        int downmost = (int) (side - 1);
+
+        Logger.log("before expand left", Severity.DEBUG);
+        // Expand left
+        for(int i = 1 /* helps with math*/ ; i <= centerX; i++) {
+            try {
+                if(newWorld[centerX - i][centerY] == TileType.OCEAN) {
+
+                    // Set chance that tile is land
+                    double chance = Math.pow(0.95, i) / 0.95;
+                    if(i > side * 2 / 3) {
+                        chance = 0;
+                    }
+
+                    if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
+                        newWorld[centerX - i][centerY] =
+                                WorldGenerator.assignRandomFlatLand();
+                    } else {
+                        i--;
+
+                        // Give leftmost a value
+                        leftmost = centerX - i;
+
+                        break; // If it's not triggered, stop expanding left
+                    }
+                }
+            } catch(ArrayIndexOutOfBoundsException e) {
+                leftmost = centerX - i + 1;
+                break;
+            }
+        }
+
+        Logger.log(EXPANDED + Integer.toString(centerX - leftmost) +
+                " tiles. before expand up", Severity.DEBUG);
+        // Expand up
+        for(int j = 1 /* helps with math*/ ; j <= centerY; j++) {
+            try {
+                if(newWorld[centerX][centerY - j] == TileType.OCEAN) {
+
+                    // Set chance that tile is land
+                    double chance = Math.pow(0.95, j) / 0.95;
+                    if(j > side * 2 / 3) {
+                        chance = 0;
+                    }
+
+                    // Determine if tile is land
+                    if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
+                        newWorld[centerX][centerY - j] =
+                                WorldGenerator.assignRandomFlatLand();
+                    } else {
+                        j--;
+
+                        // Give upmost a value
+                        upmost = centerY - j;
+
+                        break; // If it's not triggered, stop expanding up
+                    }
+                }
+            } catch(ArrayIndexOutOfBoundsException e) {
+                upmost = centerY - j + 1;
+                break;
+            }
+        }
+
+        Logger.log(EXPANDED + Integer.toString(centerY - upmost) +
+                " tiles. before expand right", Severity.DEBUG);
+        // Expand right
+        for(int ii = 1 /* helps with math*/ ; ii < side; ii++) {
+            try {
+                if(newWorld[centerX + ii][centerY] == TileType.OCEAN) {
+
+                    // Set chance that tile is land
+                    double chance = Math.pow(0.95, ii) / 0.95;
+                    if(ii > side * 2 / 3) {
+                        chance = 0;
+                    }
+
+                    if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
+                        newWorld[centerX + ii][centerY] =
+                                WorldGenerator.assignRandomFlatLand();
+                    } else {
+                        ii--;
+
+                        // Give rightmost a value
+                        rightmost = centerX + ii;
+
+                        break; // If it's not triggered, stop expanding right
+                    }
+                }
+            } catch(ArrayIndexOutOfBoundsException e) {
+                rightmost = centerX + ii - 1;
+                break;
+            }
+        }
+
+        Logger.log(EXPANDED + Integer.toString(centerX - rightmost) +
+                " tiles. before expand down", Severity.DEBUG);
+        // Expand down
+        for(int jj = 1 /* helps with math*/ ; jj < side; jj++) {
+            try {
+                if(newWorld[centerX][centerY + jj] == TileType.OCEAN) {
+
+                    // Set chance that tile is land
+                    double chance = Math.pow(0.95, jj) / 0.95;
+                    if(jj > side * 2 / 3) {
+                        chance = 0;
+                    }
+
+                    if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
+                        newWorld[centerX][centerY + jj] =
+                                WorldGenerator.assignRandomFlatLand();
+                    } else {
+                        jj--;
+
+                        // Give downmost a value
+                        downmost = centerY + jj;
+
+                        break; // If it's not triggered, stop expanding down
+                    }
+                }
+            } catch(ArrayIndexOutOfBoundsException e) {
+                downmost = centerY + jj - 1;
+                break;
+            }
+        }
+
+        Logger.log(EXPANDED + Integer.toString(centerY - downmost) +
+                " tiles. before expand squares", Severity.DEBUG);
+        // Expand continents into "squares"
+        for(int k = leftmost; k <= rightmost; k++) {
+            for(int l = upmost; l <= downmost; l++) {
+                if(newWorld[k][l] == TileType.OCEAN) {
+                    final double distance = Math.pow(Math.pow(Math.abs(centerX - k),
+                            16 / 15) + Math.pow(Math.abs(centerY - l), 16 / 15), 7 / 4);
+
+                    // Set chance that tile is land
+                    double chance = Math.pow(0.95, distance) / 0.95;
+                    if(distance > side * 2 / 3) {
+                        chance = 0;
+                    }
+
+                    if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
+                        newWorld[k][l] = WorldGenerator.assignRandomFlatLand();
+                    }
+                }
+            }
+        }
+
+        Logger.log("done with individual continent", Severity.DEBUG);
+        // Return the world plus continent
+        return newWorld;
+    }
 }
--- src/main/java/org/qme/world/gen/WorldGenerator.java
+++ src/main/java/org/qme/world/gen/WorldGenerator.java
@@ -4,7 +4,6 @@ import org.qme.io.Logger;
 import org.qme.io.Severity;
 import org.qme.utils.Direction;
 import org.qme.world.TileType;
-import org.qme.world.World;
 
 import java.util.Random;
 
@@ -15,14 +14,14 @@ import java.util.Random;
  * @since 0.1.0
  */
 public class WorldGenerator {
-	private static final Random rand = new Random();
-	private static final String EXPANDED = "expanded ";
+	static final Random rand = new Random();
+	static final String EXPANDED = "expanded ";
 
 	// The higher the constant, the less of that feature generates
 	private static final int MOUNTAIN_CONSTANT = 30;
 	private static final int RIVER_CONSTANT = 120;
 
-	private WorldGenerator() {
+	WorldGenerator() {
 		throw new IllegalStateException("World Generation");
 	}
 
@@ -54,7 +53,7 @@ public class WorldGenerator {
 			int i = rand.nextInt((int) side);
 			int j = rand.nextInt((int) side);
 			Logger.log("before individual continent", Severity.DEBUG);
-			world = WorldGenerator.addContinent(world, side, i, j);
+			world = AddContinent.addContinent(world, side, i, j);
 		}
 		
 		// Make coastal oceans into seas
@@ -109,186 +108,6 @@ public class WorldGenerator {
 		
 		// Return the ocean-world
 		return wet;
-	}
-	
-	/**
-	 * A utility function to generate and add a continent
-	 * @author santiago
-	 * @since 0.1.0
-	 * @param world The array to be modified
-	 * @param side The size of the world
-	 * @param centerX The center of the continent
-	 * @param centerY The center of the continent
-	 * @return The inputted array with a continent added
-	 */
-	private static TileType[][] addContinent(TileType[][] world,
-			double side, int centerX, int centerY) {
-
-		// Set up return world
-		TileType[][] newWorld;
-		newWorld = world;
-		
-		Logger.log("assign center tile to random", Severity.DEBUG);
-		// Assign center tile to a random non-mountain land type
-		newWorld[centerX][centerY] = WorldGenerator.assignRandomFlatLand();
-		Logger.log("centerX is: " + Integer.toString(centerX), Severity.DEBUG);
-		Logger.log("centerY is: " + Integer.toString(centerY), Severity.DEBUG);
-		
-		// Set up extreme points with most extreme values
-		int leftmost = 0;
-		int upmost = 0;
-		int rightmost = (int) (side - 1);
-		int downmost = (int) (side - 1);
-		
-		Logger.log("before expand left", Severity.DEBUG);
-		// Expand left
-		for(int i = 1 /* helps with math*/ ; i <= centerX; i++) {
-			try {
-				if(newWorld[centerX - i][centerY] == TileType.OCEAN) {
-					
-					// Set chance that tile is land
-					double chance = Math.pow(0.95, i) / 0.95;
-					if(i > side * 2 / 3) {
-						chance = 0;
-					}
-					
-					if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
-						newWorld[centerX - i][centerY] =
-								WorldGenerator.assignRandomFlatLand();
-					} else {
-						i--;
-						
-						// Give leftmost a value
-						leftmost = centerX - i;
-						
-						break; // If it's not triggered, stop expanding left
-					}
-				}
-			} catch(ArrayIndexOutOfBoundsException e) {
-				leftmost = centerX - i + 1;
-				break;
-			}
-		}
-		
-		Logger.log(EXPANDED + Integer.toString(centerX - leftmost) +
-				" tiles. before expand up", Severity.DEBUG);
-		// Expand up
-		for(int j = 1 /* helps with math*/ ; j <= centerY; j++) {
-			try {
-				if(newWorld[centerX][centerY - j] == TileType.OCEAN) {
-					
-					// Set chance that tile is land
-					double chance = Math.pow(0.95, j) / 0.95;
-					if(j > side * 2 / 3) {
-						chance = 0;
-					}
-					
-					// Determine if tile is land
-					if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
-						newWorld[centerX][centerY - j] =
-								WorldGenerator.assignRandomFlatLand();
-					} else {
-						j--;
-						
-						// Give upmost a value
-						upmost = centerY - j;
-						
-						break; // If it's not triggered, stop expanding up
-					}
-				}
-			} catch(ArrayIndexOutOfBoundsException e) {
-				upmost = centerY - j + 1;
-				break;
-			}
-		}
-		
-		Logger.log(EXPANDED + Integer.toString(centerY - upmost) +
-				" tiles. before expand right", Severity.DEBUG);
-		// Expand right
-		for(int ii = 1 /* helps with math*/ ; ii < side; ii++) {
-			try {
-				if(newWorld[centerX + ii][centerY] == TileType.OCEAN) {
-					
-					// Set chance that tile is land
-					double chance = Math.pow(0.95, ii) / 0.95;
-					if(ii > side * 2 / 3) {
-						chance = 0;
-					}
-					
-					if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
-						newWorld[centerX + ii][centerY] =
-								WorldGenerator.assignRandomFlatLand();
-					} else {
-						ii--;
-						
-						// Give rightmost a value
-						rightmost = centerX + ii;
-						
-						break; // If it's not triggered, stop expanding right
-					}
-				}
-			} catch(ArrayIndexOutOfBoundsException e) {
-				rightmost = centerX + ii - 1;
-				break;
-			}
-		}
-		
-		Logger.log(EXPANDED + Integer.toString(centerX - rightmost) +
-				" tiles. before expand down", Severity.DEBUG);
-		// Expand down
-		for(int jj = 1 /* helps with math*/ ; jj < side; jj++) {
-			try {
-				if(newWorld[centerX][centerY + jj] == TileType.OCEAN) {
-					
-					// Set chance that tile is land
-					double chance = Math.pow(0.95, jj) / 0.95;
-					if(jj > side * 2 / 3) {
-						chance = 0;
-					}
-					
-					if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
-						newWorld[centerX][centerY + jj] =
-								WorldGenerator.assignRandomFlatLand();
-					} else {
-						jj--;
-						
-						// Give downmost a value
-						downmost = centerY + jj;
-						
-						break; // If it's not triggered, stop expanding down
-					}
-				}
-			} catch(ArrayIndexOutOfBoundsException e) {
-				downmost = centerY + jj - 1;
-				break;
-			}
-		}
-		
-		Logger.log(EXPANDED + Integer.toString(centerY - downmost) +
-				" tiles. before expand squares", Severity.DEBUG);
-		// Expand continents into "squares"
-		for(int k = leftmost; k <= rightmost; k++) {
-			for(int l = upmost; l <= downmost; l++) {
-				if(newWorld[k][l] == TileType.OCEAN) {
-					final double distance = Math.pow(Math.pow(Math.abs(centerX - k),
-							16 / 15) + Math.pow(Math.abs(centerY - l), 16 / 15), 7 / 4);
-					
-					// Set chance that tile is land
-					double chance = Math.pow(0.95, distance) / 0.95;
-					if(distance > side * 2 / 3) {
-						chance = 0;
-					}
-					
-					if(rand.nextInt(10000) < Math.floor(10000 * chance)) {
-						newWorld[k][l] = WorldGenerator.assignRandomFlatLand();
-					}
-				}
-			}
-		}
-
-		Logger.log("done with individual continent", Severity.DEBUG);
-		// Return the world plus continent
-		return newWorld;
 	}
 	
 	/**
@@ -440,7 +259,7 @@ public class WorldGenerator {
 	 * @since 0.1.0
 	 * @return A random non-mountain, non-water tile
 	 */
-	private static TileType assignRandomFlatLand() {
+	static TileType assignRandomFlatLand() {
 		int type = rand.nextInt(8);
 		if (type == 0 || type == 4) {
 			return TileType.PLAINS;
